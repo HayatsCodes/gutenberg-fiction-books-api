c355dbf6ef200c8cf35e37af75cfc0b9974b082a
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-28 22:44:38 +0100

    Improving performance

---
+++ src/utils/mongo.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const MONGO_URL = 'mongodb+srv://gutenberg-api:<password>@gutenberg-fiction-books.ennxkis.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connection('open', () => {
+    console,log('MongoDB connection ready');
+});
+
+mongoose.connection().on('error', (err) => {
+    console.error(err);
+});
+
+mongoose.set('strictQuery', false);
+
