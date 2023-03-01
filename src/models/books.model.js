534def0757ee251291a6303818d0847921a67a79
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-01 23:12:07 +0100

    Refactoring the code

--- src/models/books.model.js
+++ src/models/books.model.js
@@ -75,7 +75,7 @@ async function saveBook(book) {
    
 }
 
-async function getAllBooks(skip, limit) {
+async function getAllBooks(skip = 0, limit) {
     console.log('Books found!')
     return await books
     .find({}, {'_id': 0, '__v': 0})
