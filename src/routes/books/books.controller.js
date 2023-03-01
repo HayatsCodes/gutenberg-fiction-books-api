c2cb8ecb15e61ba9c604ccd54f1914c01ec34265
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-01 14:03:32 +0100

    Updating the code

--- src/routes/books/books.controller.js
+++ src/routes/books/books.controller.js
@@ -1,5 +1,5 @@
 const { getAllBooks } = require('../../models/books.model');
 
-async function http() {
-
+async function httpGetAllBooks(req, res) {
+    return res.status(200).json(await getAllBooks())
 }
