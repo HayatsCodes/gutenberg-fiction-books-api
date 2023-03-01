84568ba99167ae5549df5b6d7604366d6460e3c0
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-01 14:20:24 +0100

    Refactoring the code

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+const bookRouter = require('./routes/books/books.router');
+
 app.use(morgan('tiny'));
-app.use('books')
+app.use('books', bookRouter);
 
 
 module.exports = app;
