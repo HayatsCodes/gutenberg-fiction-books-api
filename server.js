5e672509b4ab47a2a8fe40a2f446d41d029a4db8
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-01 11:16:47 +0100

    Refactoring the code

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ const http = require('http');
 
 const app = require('./src/app');
 const mongoConnect = require('./src/utils/mongo');
+const { loadBooks } = require('./src/models/books.model');
 
 const server = http.createServer(app);
 
 async function startServer() {
     await mongoConnect();
+    await loadBooks();
 
     server.listen(5000, () => {
         console.log('Listening on port 5000');
     });
 }
+
+startServer();
