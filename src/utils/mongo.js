76e71ba46f5f30eb801f17de93de6f87ba07f55e
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-01 11:21:35 +0100

    Adding a new feature

--- src/utils/mongo.js
+++ src/utils/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL = 'mongodb+srv://gutenberg-api:<password>@gutenberg-fiction-books.ennxkis.mongodb.net/?retryWrites=true&w=majority';
+const MONGO_URL = 'mongodb+srv://gutenberg-api:6qOnacZOtlJCbIrt@gutenberg-fiction-books.ennxkis.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose.connection.once('open', () => {
     console,log('MongoDB connection ready');
