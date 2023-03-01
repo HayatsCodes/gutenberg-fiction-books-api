b748500a4ed890307c44378f0a1f96829ff6cd8e
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-01 22:37:53 +0100

    Improving performance

--- src/routes/books/books.controller.js
+++ src/routes/books/books.controller.js
@@ -2,7 +2,8 @@ const { getAllBooks } = require('../../models/books.model');
 
 async function httpGetAllBooks(req, res) {
     console.log('getting books...')
-    cons
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) 
     return res.status(200).json(await getAllBooks());
 }
 
