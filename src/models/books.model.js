0df2455b498005587a13feda3a5d5da964ee9ca4
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-01 10:40:39 +0100

    Updating the code

--- src/models/books.model.js
+++ src/models/books.model.js
@@ -43,7 +43,7 @@ function loadBooks() {
     });
 }
 async function saveBook(book) {
-    const BOOK_URL = ''
+    const BOOK_URL = 'http://www.gutenberg.org/ebooks'
     await books.updateOne({
         id: book['Text#']
     }, {
