ea68d5e25159b5651c6a085850c7551f8bd84db1
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-01 15:00:18 +0100

    Updating the documentation

--- src/app.js
+++ src/app.js
@@ -5,6 +5,6 @@ const app = express();
 const bookRouter = require('./routes/books/books.router');
 
 app.use(morgan('tiny'));
-app.use('books', bookRouter);
+app.use('/books', bookRouter);
 
 module.exports = app;
