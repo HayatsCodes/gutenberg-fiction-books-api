6bb9853f699e9d7e899c7cde54d99dbf443f265e
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-01 09:58:50 +0100

    Updating the code

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const http = require('http');
 
 const app = require('./src/app');
+const mongoConnect = require('./src/utils/mongo');
 
 const server = http.createServer(app);
 
-mongo
+function startServer() {}
+mongoConnect();
 
 server.listen(5000, () => {
     console.log('Listening on port 5000');
