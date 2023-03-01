b281cc37d3591e4a1aa3bdfde4fe5162ab1f4646
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-01 23:09:09 +0100

    Refactoring the code

--- src/models/books.model.js
+++ src/models/books.model.js
@@ -79,7 +79,7 @@ async function getAllBooks(skip, limit) {
     console.log('Books found!')
     return await books
     .find({}, {'_id': 0, '__v': 0})
-    .sort({ order: 1})
+    .sort({ order_id: 1})
     .skip(skip)
     .limit(limit)
 }
