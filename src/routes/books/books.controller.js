de5757f064bfe7e3607dfb56e58fa433323f7c9b
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-01 22:38:54 +0100

    Adding a new feature

--- src/routes/books/books.controller.js
+++ src/routes/books/books.controller.js
@@ -3,7 +3,7 @@ const { getAllBooks } = require('../../models/books.model');
 async function httpGetAllBooks(req, res) {
     console.log('getting books...')
     const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) 
+    const limit = Number(req.query.limit) || 20;
     return res.status(200).json(await getAllBooks());
 }
 
