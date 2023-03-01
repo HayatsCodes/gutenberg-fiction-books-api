32e2f786dce95f4c5e7e4864735236994622805c
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-01 14:09:05 +0100

    Improving performance

--- src/routes/books/books.router.js
+++ src/routes/books/books.router.js
@@ -3,4 +3,6 @@ const httpGetAllBooks = require('./books.controller');
 
 const bookRouter = express.Router();
 
-bookRouter.get('/books')
+bookRouter.get('/books', httpGetAllBooks);
+
+module.exports = bookRouter;
