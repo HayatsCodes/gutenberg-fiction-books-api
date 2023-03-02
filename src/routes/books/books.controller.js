a6394dd0b853e9ce0133e638a42b6da04509d49c
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-02 01:42:03 +0100

    Fixing a bug

--- src/routes/books/books.controller.js
+++ src/routes/books/books.controller.js
@@ -1,4 +1,4 @@
-const { getAllBooks, getBookById} = require('../../models/books.model');
+const { getAllBooks, getBookById, getBookByOrderId } = require('../../models/books.model');
 
 async function httpGetAllBooks(req, res) {
     console.log('getting books...')
@@ -11,13 +11,23 @@ async function httpGetAllBooks(req, res) {
 }
 
 async function httpGetBook(req, res) {
-    const id = req.params.id;
-    const byOrderId = req.query.order_id === 'true';
-    const book = await getBookById(id, byOrderId);
-    if (!book) {
-        return res.status(404).json({message: 'Book not found'});
+    const id = req.params.id
+
+
+    if (req.query.order_id) {
+        const book = await getBookByOrderId(id);
+        if (!book) {
+            return res.status(400).json('Book not found');
+        }
+        return res.status(200).json(book);
+    } else {
+        const book = await getBookById(id);
+        if (!book) {
+            return res.status(400).json('Gutenberg book id not found');
+        }
+        return res.status(200).json(book);
+
     }
-    return res.status(200).json(book);
 }
 
 module.exports = {
