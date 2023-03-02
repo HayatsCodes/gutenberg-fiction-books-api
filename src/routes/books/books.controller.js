3a9d66f4fa146944b28dc0a98695dde874d8d463
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-02 01:25:12 +0100

    Updating the code

--- src/routes/books/books.controller.js
+++ src/routes/books/books.controller.js
@@ -14,7 +14,10 @@ async function httpGetBook(req, res) {
     const id = req.params.id;
     const byOrderId = req.query.order_id === true;
     const book = await getBookById(id, byOrderId);
-    if (!)
+    if (!book) {
+        return res.status(404).json({message: 'Book not found'});
+    }
+    return res.status(200).json(book);
 }
 
 module.exports = {
