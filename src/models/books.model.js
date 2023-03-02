5254ac0542d11cbff89160ebfe1d487a7e47aafe
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-02 01:33:45 +0100

    Updating the documentation

--- src/models/books.model.js
+++ src/models/books.model.js
@@ -87,9 +87,9 @@ async function getAllBooks(skip, limit) {
 
 async function getBookById(id, byOrderId) {
     if (byOrderId) {
-        return await books.findOne({order_id: +id}, {'_id': 0, '__v': 0});
+        return await books.find({order_id: +id}, {'_id': 0, '__v': 0});
     }
-    return await books.findOne({gutenberg_id: id}, {'_id': 0, '__v': 0});
+    return await books.find({gutenberg_id: id}, {'_id': 0, '__v': 0});
 }
 
 
