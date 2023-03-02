12b9e77b0ec7a60f450e96ca6360ec64956d4570
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-02 14:22:04 +0100

    Updating the code

--- src/routes/books/books.controller.js
+++ src/routes/books/books.controller.js
@@ -16,7 +16,7 @@ async function httpGetAllBooks(req, res) {
 
 
     if (order_id) {
-        console.log('Getting book by orderId');
+        console.log('Getting book by order Id');
         const book = await getBookByOrderId(order_id);
         if (book) {
             return res.status(200).json(book);
