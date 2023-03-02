d32e2b9d1116945c35ca8b0aa9a8799f2c314d6d
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-02 01:34:47 +0100

    Refactoring the code

--- src/models/books.model.js
+++ src/models/books.model.js
@@ -87,6 +87,7 @@ async function getAllBooks(skip, limit) {
 
 async function getBookById(id, byOrderId) {
     if (byOrderId) {
+        console.log('BY ORDER ID');
         return await books.find({order_id: +id}, {'_id': 0, '__v': 0});
     }
     return await books.find({gutenberg_id: id}, {'_id': 0, '__v': 0});
