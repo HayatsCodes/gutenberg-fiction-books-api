5e6f39c6e4d5582b0cb48483bc9a55aa04fc02b3
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-03 17:09:10 +0100

    Updating the code

--- src/models/books.model.js
+++ src/models/books.model.js
@@ -75,23 +75,6 @@ async function saveBook(book) {
    
 }
 
-async function getAllBooks(skip, limit) {
-    console.log('Books found!')
-    return await books
-    .find({}, {'_id': 0, '__v': 0})
-    .sort({ order_id: 1})
-    .skip(skip)
-    .limit(limit);
-}
-
-async function getBookById(id) {
-    console.log('id: ', id);
-    return await books.find({gutenberg_id: id}, {'_id': 0, '__v': 0});
-}
-
-async function getBookByOrderId(id) {
-    return await books.find({order_id: id}, {'_id': 0, '__v': 0});
-}
 
 module.exports = {
     loadBooks,
